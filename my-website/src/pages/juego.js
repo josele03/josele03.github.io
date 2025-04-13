dd1d2820068bf4666ea5678a3b0ec17dd166ee9c
jose.marquez <jose.marquez@alumnos.upm.es>
2025-04-13 19:18:15 +0200

    ...

--- my-website/src/pages/juego.js
+++ my-website/src/pages/juego.js
@@ -2,9 +2,12 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import AppRubix from '../components/Rubix/AppRubix.js';
 import '../css/rubix.css'; // Importa los estilos de RubixCube
+import BrowserOnly from '@docusaurus/BrowserOnly';
+
 
 export default function RubixCubePage() {
   return (
+    <BrowserOnly> 
     <Layout
       title="RubixCube"
       description="Pantalla interactiva del Cubo de Rubik">
@@ -12,5 +15,6 @@ export default function RubixCubePage() {
         <AppRubix /> {/* Renderiza el modelo interactivo */}
       </main>
     </Layout>
+    </BrowserOnly>
   );
 }
