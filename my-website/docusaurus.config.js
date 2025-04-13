2f4ae5d40b83216925e50abcf9b8204dfcbba914
josele03 <jose.marquez@alumnos.upm.es>
2025-04-13 21:50:54 +0200

    ...

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://josele03.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/josele03.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
